8ce32ff08b91c7293f19dc0579b485c08cb7c267
wayungi <fwayungi@gmail.com>
2022-08-11 22:12:47 +0300

    Add form

---
+++ src/components/partials/form.js
@@ -0,0 +1,17 @@
+/** @format */
+
+const Form = () => (
+  <form>
+    <div>
+      <input type="text" required id="title" placeholder="Book Title" />
+    </div>
+    <div>
+      <input type="text" required id="author" placeholder="Author" />
+    </div>
+    <div>
+      <input type="Buuton" value="Add Book" />
+    </div>
+  </form>
+);
+
+export default Form;
--- src/components/partials/form.jsx
+++
@@ -1,3 +0,0 @@
-const Form = () => <form />;
-
-export default Form;
