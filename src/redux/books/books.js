3c00ab860198b2ab2f12bbd7bf37302bf45d8bca
wayungi <fwayungi@gmail.com>
2022-08-16 21:54:02 +0300

    Add remove & add book actions

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -22,3 +22,20 @@ const removeBookReducer = (state = [], action) => {
       state;
   }
 };
+
+/*
+action has type, and data
+
+*/
+const actionAddBook = (bookObj) => {
+  const { id, title, completed } = bookObj;
+  const action = { type: ADD_BOOK, data: { id, title, completed } };
+  return action;
+};
+
+const actionRemoveBook = (id) => {
+  const action = { type: REMOVE_BOOK, data: id };
+  return action;
+};
+
+export { actionAddBook, actionRemoveBook, removeBookReducer, addBookReducer };
