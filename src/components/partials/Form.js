e11e3c1dc043c192970537ddb8ab92691a5a2ccd
wayungi <fwayungi@gmail.com>
2022-08-18 22:35:13 +0300

    Implement add book

--- src/components/partials/Form.js
+++ src/components/partials/Form.js
@@ -1,41 +1,63 @@
 /** @format */
 
-// import { useDispatch } from 'react-redux';
-// import { useState } from 'react';
-// import { actionAddBook } from '../../redux/books/books';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { actionAddBook } from '../../redux/books/books';
 
-const Form = () => (
-  // const [title, setTitle] = useState('');
-  // const dispatch = useDispatch();
+const Form = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const dispatch = useDispatch();
+
+  const handleTitleInput = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleAuthorInput = (e) => {
+    setAuthor(e.target.value);
+  };
   // setTitle();
 
-  <form>
-    <div>
-      <h1>Add New BooK</h1>
-    </div>
-    <div>
-      <input
-        type="text"
-        required
-        id="title"
-        placeholder="Book Title"
-        // value={title}
-      />
-    </div>
-    <div>
-      <input type="text" required id="author" placeholder="Author" />
-    </div>
-    <div>
-      <button
-        type="button"
-        // onClick={() => {
-        //   dispatch(actionAddBook({ title }));
-        // }}
-      >
-        Add Book
-      </button>
-    </div>
-  </form>
-);
+  return (
+    <form>
+      <div>
+        <h1>Add New BooK</h1>
+      </div>
+      <div>
+        <input
+          type="text"
+          required
+          id="title"
+          placeholder="Book Title"
+          value={title}
+          name="title"
+          onChange={handleTitleInput}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          required
+          id="author"
+          placeholder="Author"
+          value={author}
+          name="author"
+          onChange={handleAuthorInput}
+        />
+      </div>
+      <div>
+        <button
+          type="button"
+          onClick={() => {
+            dispatch(actionAddBook({ id: uuidv4, title, completed: false }));
+          }}
+        >
+          Add Book
+        </button>
+      </div>
+    </form>
+  );
+};
 
 export default Form;
