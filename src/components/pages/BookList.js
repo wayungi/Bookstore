d1a6309ddf5b101db8455b1f440fd812d82ad802
wayungi <fwayungi@gmail.com>
2022-08-19 19:25:18 +0300

    Fetch books data from API

--- src/components/pages/BookList.js
+++ src/components/pages/BookList.js
@@ -1,10 +1,20 @@
 /** @format */
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import Form from '../partials/Form';
 import Book from '../partials/Book';
 
 const BookList = () => {
   const books = useSelector((state) => state.books);
+  // runs on page load
+  useEffect(() => {
+    fetch(
+      'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/1EGh4k8reaqTuMnlef5i/books',
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <div>
       {books.map((book) => (
