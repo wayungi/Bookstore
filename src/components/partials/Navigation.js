316bbee69d6f3f07cdc04fb6425bd40f3307d42e
wayungi <fwayungi@gmail.com>
2022-08-20 13:14:05 +0300

    Add flex display to navbar left

--- src/components/partials/Navigation.js
+++ src/components/partials/Navigation.js
@@ -4,17 +4,21 @@ import styles from './Navigation.module.css';
 
 const Navigation = () => (
   <div className={styles.navbar}>
-    <div>
-      <h1>Bookstore CMS</h1>
+    <div className={styles.navbarLeft}>
+      <div>
+        <h1>Bookstore CMS</h1>
+      </div>
+      <ul>
+        <li>
+          <Link to="/">BOOKS</Link>
+        </li>
+        <li>
+          <Link to="/category">CATEGORIES</Link>
+        </li>
+      </ul>
     </div>
-    <ul>
-      <li>
-        <Link to="/">BOOKS</Link>
-      </li>
-      <li>
-        <Link to="/category">CATEGORIES</Link>
-      </li>
-    </ul>
+
+    <div className={styles.navbarRight}>Logo</div>
   </div>
 );
 
