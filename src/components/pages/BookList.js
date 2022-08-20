915c9a35671ce25cd5c097e497a2e9db324b6cd6
wayungi <fwayungi@gmail.com>
2022-08-20 12:29:53 +0300

    Add navigation bar to BookList page

--- src/components/pages/BookList.js
+++ src/components/pages/BookList.js
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 import { actionGetBooks } from '../../redux/books/books';
 import Form from '../partials/Form';
 import Book from '../partials/Book';
+import Navigation from '../partials/Navigation';
 
 const BookList = () => {
   const books = useSelector((state) => state.books);
@@ -13,14 +14,17 @@ const BookList = () => {
   }, []);
 
   return (
-    <ul>
-      {books.map((book) => (
-        <li key={book.item_id}>
-          <Book book={book} />
-        </li>
-      ))}
-      <Form />
-    </ul>
+    <div>
+      <Navigation />
+      <ul>
+        {books.map((book) => (
+          <li key={book.item_id}>
+            <Book book={book} />
+          </li>
+        ))}
+        <Form />
+      </ul>
+    </div>
   );
 };
 
---
+++ src/components/partials/Navigation.js
@@ -0,0 +1,3 @@
+const Navigation = () => (<div>Main bar</div>);
+
+export default Navigation;
